cecf0506067c2332102f2690f7a937c4155c9044
Paul Wagner <p.wagner1120@icloud.com>
2023-01-18 09:41:09 -0600

    adding constructor and get methods to contact file

--- src/Contact.java
+++ src/Contact.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Contact {
 
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+
 
 
 }
